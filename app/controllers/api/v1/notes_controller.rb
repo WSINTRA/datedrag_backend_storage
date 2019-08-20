90139a53c00e81c7d7a51fd4e5de8f26f7103475
WSINTRA <will.simpson85@gmail.com>
2019-08-20 16:57:30 -0400

    fixed bug that would save memos only by date and not by current user

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -8,8 +8,9 @@ class Api::V1::NotesController < ApplicationController
 
 	def create
 		#will first check if the date has a note, if not create one or edit the current one
+		
 		get_user = User.find(note_params["user_id"])
-		get_memo = Memo.where(date: note_params["date"])
+		get_memo = get_user.memos.where(date: note_params["date"])
 		
 		if get_memo.length > 0
 			get_memo.update_all(note: note_params["note"]) 
